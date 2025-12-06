ce4ad71f2b80508e87f547ca85f45e3264fdd4a6
AkieGZH <gzhkie@qq.com>
2025-12-06 23:36:01 +0800

    Optimization.

--- models/net.py
+++ models/net.py
@@ -11,23 +11,34 @@ class Net(nn.Module):
 
         self.net = nn.Sequential(
             # (batch_size, 1, 28, 28)
-            nn.Conv2d(1, 128, kernel_size=3, stride=1, padding=0),
-            # (batch_size, 128, 26, 26)
+            nn.Conv2d(1, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            # (batch_size, 128, 28, 28)
             nn.MaxPool2d(2, 2),
-            # (batch_size, 128, 13, 13)
-            nn.Dropout(0.25),
-            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=0),
-            # (batch_size, 256, 11, 11)
+            # (batch_size, 128, 14, 14)
+            nn.Dropout2d(0.25),
+
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            # (batch_size, 256, 14, 14)
             nn.MaxPool2d(2, 2),
-            # (batch_size, 256, 5, 5)
-            nn.Dropout(0.25),
-            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=0),
+            # (batch_size, 256, 7, 7)
+            nn.Dropout2d(0.25),
+
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
             # (batch_size, 512, 3, 3)
-            nn.Dropout(0.4),
+            nn.Dropout2d(0.4),
+
             nn.Flatten(),
             # (batch_size, 512 * 3 * 3)
             nn.Linear(512 * 3 * 3, 128),
-            nn.Dropout(0.3),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Dropout(0.5),
             nn.Linear(128, 10)
         )
 
