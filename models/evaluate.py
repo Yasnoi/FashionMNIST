51dd2083564fc3e24fdc4504fcb5d47c6491f04a
Yasnoi <gzhkie@qq.com>
2025-12-07 22:14:08 +0800

    Optimization.

--- models/evaluate.py
+++ models/evaluate.py
@@ -2,6 +2,7 @@ import torch
 import torch.optim as optim
 from torch import nn
 import os
+import csv
 
 from models.net import Net
 from utils.data_loader import fashion_mnist_data_loader
@@ -23,11 +24,17 @@ class EvaluateNet:
 
         self.criterion = nn.CrossEntropyLoss()
 
+        self.results_save_path = os.path.join(config['output']['result_dir'], "submission.csv")
+
     def evaluate(self):
         self.model.eval()
         test_loss = 0
         correct_predictions = 0
         total_samples = 0
+
+        result = []
+        current_id = 0
+
         with torch.no_grad():
             for batch_idx, (data, target) in enumerate(self.data_loader):
                 data, target = data.to(self.device), target.to(self.device)
@@ -41,7 +48,18 @@ class EvaluateNet:
                 correct_predictions += predictions.eq(target.view_as(predictions)).sum().item()
                 total_samples += target.shape[0]
 
+                pred_list = predictions.cpu().numpy().flatten()
+                for label in pred_list:
+                    result.append([current_id, label])
+                    current_id += 1
+
         total_loss = test_loss / total_samples
         total_accuracy = 100. * correct_predictions / total_samples
         print(f'Running on test set...\n'
               f'Test Loss: {total_loss:.4f}, Test Accuracy: {total_accuracy:.2f}%')
+
+        with open(self.results_save_path, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['Id', 'Label'])
+            writer.writerows(result)
+        print(f'Results saved to {self.results_save_path}')
